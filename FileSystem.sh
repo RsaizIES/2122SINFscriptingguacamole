4a7ef6cb0cc799cfee8778e15094e1f6f8310dec
Jorge <jorgar.alu@iesbenigaslo.es>
2022-04-08 11:53:40 +0200

    modded FileSystem.bash, addin the functions

--- FileSystem.sh
+++ FileSystem.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 #parse a stringh , get onde by one all characters
-#hi
+if [ -z "$1" ]
+    then echo "the parameter its empty"
+    echo "Try to put a valid Directory"
+    echo " '.' /directory "
+    exit 1
+    fi
+Dir=$1
+ls $Dir -1 > buffer.tmp
+while read Element
+do
+    #check
+    if [ -d "$Dir/$Element" ]; then
+        echo "$Element is a directory"
+            echo  "$( ls -lh $Dir/$Element )"
+        elif [ -f "$Dir/$Element" ] || [ -r "$Dir/$Element" ] && R="Can Read" || R="Can't Read" [ -w "$Dir/$Element" ] && W="Can Write" || W="Can't Write" [ -x "$Dir/$Element" ] && X="Can execute" || X="Can't execute"; then
+            echo "$Element is a file // $R,$W,$X"
+            echo  "$( ls -lh $Dir/$Element )"
+                    else
+                        echo "I don't Know whats $Element"
+
+    fi
+done < buffer.tmp
+
+rm buffer.tmp
+exit 0
